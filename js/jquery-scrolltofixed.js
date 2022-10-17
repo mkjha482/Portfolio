cab9cf2d6ffac14802122ceeb4843a8efed4a409
Mayank <mkjha482@gmail.com>
2022-10-17 17:45:55 +0530

    minor changes

--- js/jquery-scrolltofixed.js
+++ js/jquery-scrolltofixed.js
@@ -1,10 +1,4 @@
-/*
- * ScrollToFixed
- * https://github.com/bigspotteddog/ScrollToFixed
- * 
- * Copyright (c) 2011 Joseph Cava-Lynch
- * MIT license
- */
+
 (function($) {
     $.isScrollToFixed = function(el) {
         return !!$(el).data('ScrollToFixed');
